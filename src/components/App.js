07001a9e0ff00cb3d105d478deda33c62ab3b184
ARKIX\marino.londono <marino.londono@arkix.com>
2019-05-05 14:18:12 -0500

    proyecto citas react

---
+++ src/components/App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import AgregarCita from './AgregarCita';
+import ListaCitas from './ListaCitas';
+
+
+class App extends Component{
+  state={
+    citas:[]
+  }
+  componentDidMount(){
+    const citasLS=localStorage.getItem('citas');
+    if(citasLS){
+      this.setState({
+        citas: JSON.parse(citasLS)
+      })
+    }
+  }
+  componentDidUpdate(){
+    localStorage.setItem('citas',JSON.stringify(this.state.citas))
+  }
+  crearCita=(nuevaCita)=>{
+    // console.log(cita);
+    const citas=[...this.state.citas, nuevaCita];
+    console.log(citas);
+    this.setState({
+      citas
+    });
+  }
+  borrarCita=id=>{
+    // obtener copia del state
+    const citasActuales=[...this.state.citas];
+
+    // borrar elemento del state
+  const citas=citasActuales.filter(cita=>cita.id !==id);
+
+    // actualizar state
+    this.setState({
+      citas
+    })
+
+  }
+  render(){
+    return (
+      <div className="container">
+        <Header
+          titulo={'Administrador de pacientes de veterinaria'}
+        />
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita 
+              crearCita={this.crearCita}
+            />
+          </div>
+          <div className="col -md-6">
+            <ListaCitas
+              citas={this.state.citas}
+              borrarCita={this.borrarCita}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
